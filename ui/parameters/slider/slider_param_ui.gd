3a7d6e1b98b30b18997e2a24173b96267853efc0
Samet Atabek <samatbk@proton.me>
2025-02-19 12:58:16 +0300

    yeni parametreler

---
+++ ui/parameters/slider/slider_param_ui.gd
@@ -0,0 +1,26 @@
+class_name SliderParamUI
+extends HBoxContainer
+
+@export var parameter: SliderParameterRes
+
+const SCENE: PackedScene = preload("res://ui/parameters/slider/slider_param_ui.tscn")
+
+@onready var slider := $Slider
+@onready var label := $Label
+
+func _ready() -> void:
+	label.set_text(parameter.label)
+	slider.max_value = parameter.max_value
+	slider.min_value = parameter.min_value
+	slider.value =  parameter.value
+	slider.step = parameter.step
+	
+static func new_slider(param: SliderParameterRes) -> SliderParamUI:
+	var obj = SCENE.instantiate()
+	obj.parameter = param
+
+	return obj
+
+
+func _on_value_changed(value: float) -> void:
+	parameter.value = value
