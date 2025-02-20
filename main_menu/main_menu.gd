e487df7379bd775d68a9464b2458524e85e13d50
samatbk <samatbk@proton.me>
2025-02-20 19:54:45 +0300

    ürün seçme ve ana menü teması (#6)
    * urun secme ekrani yapildi
    
    * ana menu tema degisikligi

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_new_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu/new_game_menu.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_load_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu/load_game_menu.tscn")
